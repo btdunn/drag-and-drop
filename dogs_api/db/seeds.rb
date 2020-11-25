ecf080ef6f689add349a38dd99caa1b0dfbf57fd
LimitToInfinity <21stLimitless@gmail.com>
2020-11-25 11:06:23 -0700

    Adds important dog seeds.

--- dogs_api/db/seeds.rb
+++ dogs_api/db/seeds.rb
@@ -1,10 +1,13 @@
 Dog.destroy_all
 
-Dog.create({ name: "Bixby", age: 7 })
-Dog.create({ name: "Fluffy", age: 3 })
-Dog.create({ name: "Floofy", age: 6 })
-Dog.create({ name: "Fleming", age: 1 })
-Dog.create({ name: "Dory", age: 11 })
-Dog.create({ name: "Sunshine", age: 10 })
-Dog.create({ name: "Snickers", age: 13 })
-Dog.create({ name: "Ranger", age: 5 })
+Dog.create name: 'Bixby', age: 7
+Dog.create name: 'Fluffy', age: 3
+Dog.create name: 'Floofy', age: 6
+Dog.create name: 'Fleming', age: 1
+Dog.create name: 'Dory', age: 11
+Dog.create name: 'Sunshine', age: 10
+Dog.create name: 'Snickers', age: 13
+Dog.create name: 'Pepper', age: 12
+Dog.create name: 'Ranger', age: 7
+Dog.create name: 'Lima', age: 5
+Dog.create name: 'Beeko', age: 5
